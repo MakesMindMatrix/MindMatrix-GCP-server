d2cc901c9e1b9051d19cf1db2222b9a89b41ae7f
Nimish Kumar <131428877+MakesMindMatrix@users.noreply.github.com>
2025-04-04 14:42:00 +0530

    staging test

--- routes/test_batch.route.js
+++ routes/test_batch.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { createTestBatch, getTestBatch } = require('../controllers/test_batch.controller')
+const { getTest } = require('../controllers/batch.controller')
 const router = express.Router()
 
 router.route("/createTestBatch").post(createTestBatch)
 router.route("/getTestBatch").get(getTestBatch)
+router.route("/getTest").get(getTest)
 
 module.exports = router
