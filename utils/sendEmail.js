1c824626d8aeeeee39519e8c5f097200e1059b9b
Nimish Kumar <131428877+MakesMindMatrix@users.noreply.github.com>
2025-04-19 15:35:44 +0530

    mail creadential

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -14,7 +14,7 @@ const sendEmail = async (options) => {
   });
 
   const mailOptions = {
-    from: process.env.SMPT_MAIL,
+    from: process.env.SMTP_SLOT_MAIL,
     to: options.email,
     subject: options.subject,
     text: options.message,
